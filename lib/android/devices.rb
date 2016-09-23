fdd76b2df888d9729b274719d981e2181e88b141
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-09-23 17:50:57 +0100

    Added downloading of csv and relevant test

--- lib/android/devices.rb
+++ lib/android/devices.rb
@@ -1,11 +1,21 @@
 require 'android/devices/version'
+require 'csv'
 
 module Android
   module Devices
 
-    def self.update_devices
-
-      false
+    def self.update_devices(url = '')
+      csv_url = url.nil? ? 'http://storage.googleapis.com/play_public/supported_devices.csv' : url
+      begin
+        devices = CSV.parse(open(csv_url).read)
+        File.open('devices.csv','w') {|f| f.write(devices.inject([]) { |csv,row| csv << CSV.generate_line(row) }.join("").encode('UTF-8'))}
+        puts "Devices updated: #{devices.count - 1} devices available"
+        return true
+      rescue Exception
+        raise 'Unable to update devices'
+      end
     end
+
+
   end
 end
--- spec/android_devices_spec.rb
+++ spec/android_devices_spec.rb
@@ -2,7 +2,8 @@
 describe Android::Devices do
   describe 'update' do
     it 'should be able to update itself' do
-      expect(Android::Devices.update_devices).to be(true)
+      expect(Android::Devices.update_devices('spec/test_devices.csv')).to be(true)
+      expect(CSV.read('devices.csv').count).to be(4)
     end
   end
 end
