ead2bcf9db8dc70059e9dc1252da1be6af0da1ee
Kacper Madej <kacperoza@gmail.com>
2019-03-26 11:00:36 +0100

    Make lookup case-insensitive

--- lib/android/devices.rb
+++ lib/android/devices.rb
@@ -6,20 +6,65 @@ module Android
   module Devices
     PATH = File.join(File.dirname(__FILE__), '../..', 'content', 'devices.csv')
 
-    def self.devices
-      return @devices unless @devices.nil?
-      rows = CSV.read(PATH)
-      @devices = rows.each_with_object({}) do |row, devices|
-        manufacturer, marketing_name, _, model = row
-        devices[model + manufacturer] = (marketing_name || model) if model && manufacturer
+    class << self
+      # Memoizes the device list on first call
+      # @return [Hash] Hash with the devices
+      #
+      # @note has to be called before calling display_name or manufacturer_name
+      #   to initialize caches
+      # TODO: rename this to `initialize` when releasing a major version
+      def devices
+        return @devices unless @devices.nil?
+
+        rows = CSV.read(PATH)
+        load_devices(rows)
+        load_manufacturers(rows)
+
+        @devices
       end
 
-      def self.display_name(manufacturer, model)
+      # @param manufacturer [String|nil] manufacturer of the given model
+      # @param model [String|nil] phone model
+      # @return [String|nil] proper device name if found, model otherwise
+      def display_name(manufacturer, model)
         if model && manufacturer
-          @devices[model + manufacturer] || model
+          @devices[cache_key(model, manufacturer)] || model
         else
           model
         end
+      end
+
+      # @param manufacturer [String] denormalized manufacturer title
+      # @return [String] proper manufacturer title
+      # @example
+      #   Android::Devices.manufacturer_name('samsung') => 'Samsung'
+      #   Android::Devices.manufacturer_name('Htc') => 'HTC'
+      def manufacturer_name(manufacturer)
+        @manufacturers[manufacturer.downcase] || manufacturer
+      end
+
+      private
+
+      def load_devices(rows)
+        @devices = rows.each_with_object({}) do |row, devices|
+          manufacturer, marketing_name, _, model = row
+
+          next unless model && manufacturer
+
+          devices[cache_key(model, manufacturer)] = (marketing_name || model)
+        end
+      end
+
+      def load_manufacturers(rows)
+        @manufacturers = rows.each_with_object({}) do |row, manufacturers|
+          manufacturer, _ = row
+
+          manufacturers[manufacturer.downcase] = manufacturer if manufacturer
+        end
+      end
+
+      def cache_key(model, manufacturer)
+        model.downcase + manufacturer.downcase
       end
     end
   end
--- spec/android_devices_spec.rb
+++ spec/android_devices_spec.rb
@@ -5,6 +5,7 @@ require 'android/devices'
 
 describe Android::Devices do
   subject { described_class }
+
   describe '#devices' do
     it 'loads the CSV only once ' do
       expect(CSV).to receive(:read).once.and_call_original
@@ -14,24 +15,51 @@ describe Android::Devices do
   end
 
   describe '#display_name' do
+    before { subject.devices }
+
     context 'with marketing name' do
-      it 'should return the display name ' do
-        subject.devices
+      it 'returns the display name' do
         expect(subject.display_name('Samsung', 'SM-A7000')).to eq 'Galaxy A7'
+      end
+
+      it 'is case-insensitive' do
+        expect(subject.display_name('samSunG', 'sm-a7000')).to eq 'Galaxy A7'
       end
     end
 
     context 'without marketing name' do
-      it 'should return the display name ' do
-        subject.devices
+      it 'returns the display name' do
         expect(subject.display_name('Acer', 'E330')).to eq 'E330'
       end
     end
 
     context 'unknown device' do
-      it 'should return the passed model name display name ' do
-        subject.devices
+      it 'returns the passed model name display name' do
         expect(subject.display_name('Some', 'Model')).to eq 'Model'
+      end
+    end
+  end
+
+  describe '#manufacturer_name' do
+    context 'when the manufacturer is unknown' do
+      it 'returns the passed manufacturer back' do
+        expect(subject.manufacturer_name('Some')).to eq 'Some'
+      end
+    end
+
+    context 'when the manufacturer is known' do
+      manufacturers = [
+        ['ag mobile', 'AG Mobile'],
+        %w[Amtc AMTC],
+        %w[samsung Samsung],
+        %w[Iriver iRiver],
+        %w[Novo novo]
+      ]
+
+      manufacturers.each do |(input, expected)|
+        it "returns #{expected} for #{input}" do
+          expect(subject.manufacturer_name(input)).to eq expected
+        end
       end
     end
   end
